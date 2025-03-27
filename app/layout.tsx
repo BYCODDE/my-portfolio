d34af94f201be2f68d53278042b9adbfff6474e4
BYCODDE <anaraliev217@gmail.com>
2025-03-27 15:36:18 +0400

    main structure added!

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Anar's Portfolio",
+  description: "MOdern and Minimalist Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,240 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Foom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/portfolio-img-4.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://foom-app.vercel.app",
+  },
+  {
+    id: 2,
+    title: "Fasafe - control your expenses and track money",
+    des: "Platform where you can control money, create budgets and expoenses with stuning UI",
+    img: "/portfolio-img-3.svg",
+    iconLists: ["/next-1.svg", "/re.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+    link: "https://fasafe.vercel.app",
+  },
+  {
+    id: 3,
+    title: "Famazona - Online E-commerce Shop",
+    des: "Sign up, pick your favourite clothes and shop with paypal",
+    img: "/portfolio-img-2.svg",
+    iconLists: [
+      "/node-1.svg",
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/next-1.svg",
+      "/c.svg",
+    ],
+    link: "https://famazona.onrender.com",
+  },
+  {
+    id: 4,
+    title: "FaBank | Online banking platform",
+    des: "Sign up, connect your bank, make transfer and control your money",
+    img: "/portfolio-img-1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next-1.svg", "/js-1.svg"],
+    link: "https://fabank.vercel.app",
+  },
+  {
+    id: 5,
+    title: "Fizza | Online Pizza ordering",
+    des: "Sign your name, pick your craving pizza and order it",
+    img: "/portfolio-img-5.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next-1.svg", "/js-1.svg"],
+    link: "https://fizza.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Anar was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Anar's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Anar is the ideal partner.",
+    name: "Nugo Bibilashvili",
+    title: "Middle front end developer and linedevltd",
+    img: "/nugo-profile.svg",
+  },
+  {
+    quote:
+      "Defanetly Reccomend to hire Anar, Amazing teaching skills, my favorite senior developer worked with. Proffesional in coding, meetings, relatioships, delivering. Be lucky and hire him",
+    name: "Beka Eloshvili",
+    title: "developer at digital institute | vake",
+    img: "/beqa-profile.svg",
+  },
+  {
+    quote:
+      "Anar was our first developer when we opend and needed the gym, he delivered everything in just 2 week, delivered excelent work and make our gym to stand out. Defenetly recommend!",
+    name: "FreshLime",
+    title: "Gym comapny",
+    img: "/freshlime-profile.svg",
+  },
+  {
+    quote:
+      "Anar is in our company software inginare, He made our company front part special, without him we won't be same as we are and will be. Amazing softskills, delivery, He taught more then 100 students front end in our academy, it's only joy to work with him, Recomennded!",
+    name: "LineDevltd",
+    title: "Full stack web development company",
+    img: "/khazi-logo.svg",
+  },
+  {
+    quote:
+      "Collaborating with Anar was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Anar's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Anar is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of ScandiFront Technologies",
+    img: "/profile.svg",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Front End junior developer",
+    desc: "Designed and developed web applications and games with JavaScript",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Front End middle developer",
+    desc: "Working on Casino online games design, pixel perfect, developing game functionality",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Full Stack Software Engineer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies, Also work with students and juniors.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/anars-work",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/shikuzeki",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/anar-karanadze/",
+  },
+];
